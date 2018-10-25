c90b77eccfef40415f1cffb3807f0fb9b34c612a
Barry Piccinni <barry.piccinni@cica.gsi.gov.uk>
2018-10-25 11:30:55 +0100

    BP04 initial commit 251018

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary2
+{
+    public class Checkout
+    {
+        public class product
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+            public double Price { get; set; }
+
+            public product(string code, string name, double price)
+            {
+                Code = code;
+                Name = name;
+                Price = price;
+            }
+        }
+
+        private readonly List<product> items = new List<product>();
+
+        public void scan(product item)
+        {
+            items.Add(item);
+        }
+
+        public double Total()
+        {
+            double total = 0.00;
+            int tea = 0;
+            int strawb = 0;
+            foreach (product p in items)
+            {
+                switch (p.Code)
+                {
+                    case "FR1":
+                        tea++;
+                        if (tea % 2 == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            total += p.Price;
+                            break;
+                        }
+                        
+                    case "SR1":
+                        strawb++;
+                        total += p.Price;
+                        if (strawb > 2)
+                        {
+                            total -= strawb * 0.50;
+                        }
+                        break;
+                    default:
+                        total += p.Price;
+                        break;
+                }
+            }
+            return total;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Checkout client = new Checkout();
+        product tea = new product("FR1", "FruitTea", 3.11);
+        product strawberries = new product("SR1", "Strawberries", 5.00);
+        product coffee = new product("CF1", "Coffee", 11.23);
+
+
+        [TestMethod]
+        public void test1()
+        {
+            Double Expected = 22.45;
+
+            client.scan(tea);
+            client.scan(strawberries);
+            client.scan(tea);
+            client.scan(tea);
+            client.scan(coffee);
+
+            Double Actual = client.Total();
+
+            Assert.AreEqual(Actual, Expected);
+        }
+
+        [TestMethod]
+        public void test2()
+        {
+            Double Expected = 3.11;
+
+            client.scan(tea);
+            client.scan(tea);
+
+            Double Actual = client.Total();
+
+            Assert.AreEqual(Actual, Expected);
+        }
+
+        [TestMethod]
+        public void test3()
+        {
+            Double Expected = 16.61;
+
+            client.scan(strawberries);
+            client.scan(strawberries);
+            client.scan(tea);
+            client.scan(strawberries);
+
+            Double Actual = client.Total();
+
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
